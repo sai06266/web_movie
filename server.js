4c73a645090cdc0aaf71497b7785a128d701fb6c
jaemin <sai06266@naver.com>
2023-12-15 10:09:38 +0900

    로그인 구현

--- server.js
+++ server.js
@@ -2,12 +2,34 @@ const express = require('express');
 const path = require('path');
 const cheerio = require('cheerio');
 const request = require('request');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'main')));
+
+// Body Parser 미들웨어 추가
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Express 세션 설정
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// MySQL 연결 설정
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'login',
+});
 
 // 영화 순위 
 app.get('/movie-rankings', (req, res) => {
@@ -73,6 +95,108 @@ app.get('/new-movies', (req, res) => {
         }
     });
 });
+
+//로그인 구현
+function restrict(req, res, next) {
+    if (req.session.loggedin) {
+    next();
+    } else {
+    req.session.error = 'Access denied!';
+    res.sendFile(path.join(__dirname + '/main/login.html'));
+    }
+}
+
+app.use('/', function(request, response, next) {
+    if ( request.session.loggedin == true || request.url == "/login" || request.url == "/register" ) {
+    next();
+    }
+    else {
+    response.sendFile(path.join(__dirname + '/main/login.html'));
+    }
+});
+
+app.get('/login', function(request, response) {
+    response.sendFile(path.join(__dirname + '/main/login.html'));
+});
+
+app.post('/login', function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+
+    if (username && password) {
+        connection.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            if (error) {
+                console.error(error);
+                response.status(500).json({ error: 'Internal Server Error' });
+                return;
+            }
+
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+
+                // 로그인이 성공하면 사용자명을 JSON으로 클라이언트에 전달
+                response.sendFile(path.join(__dirname, '/main/index.html'));
+            } else {
+                // 로그인 실패 시 loginerror로
+                response.sendFile(path.join(__dirname, '/main/loginerror.html'));
+            }
+        });
+    } else {
+        response.status(400).json({ error: 'ID와 패스워드를 입력하세요!' });
+    }
+});
+
+app.get('/register', function(request, response) {
+    response.sendFile(path.join(__dirname + '/main/register.html'));
+});
+
+app.post('/register', function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    var password2 = request.body.password2;
+    var email = request.body.email;
+    console.log(username, password, email);
+
+    if (username && password && email) {
+        connection.query('SELECT * FROM user WHERE username = ? AND password = ? AND email = ?', [username, password, email], function(error, results, fields) {
+            if (error) throw error;
+
+            if (results.length <= 0) {
+                connection.query('INSERT INTO user (username, password, email) VALUES(?,?,?)', [username, password, email], function (error, data) {
+                    if (error) {
+                        console.log(error);
+                        response.send('오류 발생. 다시 시도하세요.');
+                    } else {
+                        console.log(data);
+                        response.redirect('/login.html');
+                    }
+                });
+            } else {
+                response.redirect('/main/index.html');
+            }
+        });
+    } else {
+        response.send('Please enter User Information!');
+        response.end();
+    }
+});
+
+app.get('/logout', function(request, response) {
+    request.session.loggedin = false;
+    response.send('<center><H1>Logged Out.</H1><H1><a href="/index.html">Goto Home</a></H1></center>');
+    response.end();
+});
+
+app.get('/index', restrict, function(request, response) {
+    if (request.session.loggedin) {
+        response.sendFile(path.join(__dirname + '/index.html'));
+    } else {
+        response.send('Please login to view this page!');
+        response.end();
+    }
+});
+
 app.listen(port, () => {
-    console.log(`서버가 http://localhost:${port} 포트에서 실행 중입니다.`);
+console.log(`서버가 http://localhost:${port} 포트에서 실행 중입니다.`);
 });
